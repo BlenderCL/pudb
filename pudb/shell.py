a6467f9476b2d3fee86943ac85aac3c8b08ec325
Wirefall <cristian@blender.cl>
2022-07-26 23:16:30 -0400

    bpython urwid fix

--- pudb/shell.py
+++ pudb/shell.py
@@ -110,14 +110,16 @@ def run_classic_shell(globals, locals):
 def run_bpython_shell(globals, locals):
     ns = SetPropagatingDict([locals, globals], locals)
 
+    import bpython.urwid
+    bpython.urwid.main(args=[], locals_=ns)
     # import bpython
     # bpython.embed(args=[], locals_=ns)
-    try:
-        import bpython.cli
-        bpython.cli.main(args=[], locals_=ns)
-    except:
-        import bpython.curtsies
-        bpython.curtsies.main(args=[], locals_=ns)
+    #try:
+    #    import bpython.cli
+    #    bpython.cli.main(args=[], locals_=ns)
+    #except:
+    #    import bpython.curtsies
+    #    bpython.curtsies.main(args=[], locals_=ns)
 
 
 # {{{ ipython
